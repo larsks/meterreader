42cbcfec3a53e125676ad9b70954abf032d7a92c
Lars Kellogg-Stedman <lars@oddbit.com>
2025-12-04 21:27:03 -0500

    Newer versions of rtlamr include a protocol type field

--- models.py
+++ models.py
@@ -2,7 +2,11 @@ from pydantic import BaseModel
 from pydantic import ConfigDict
 from pydantic import Field
 
+from enum import StrEnum
 
+
+# This handles both SCM and SCM+ messages through the magic
+# of pydantic aliases.
 class SCMMessage(BaseModel):
     model_config: ConfigDict = ConfigDict(validate_by_name=True, validate_by_alias=True)
     ID: int = Field(alias="EndpointID")
@@ -10,7 +14,14 @@ class SCMMessage(BaseModel):
     Consumption: int
 
 
+class MessageType(StrEnum):
+    UNKNOWN = "<unknown>"
+    SCM = "SCM"
+    SCMPLUS = "SCM+"
+
+
 class Reading(BaseModel):
     Time: str
     Offset: int
+    Type: MessageType = MessageType.UNKNOWN
     Message: SCMMessage
